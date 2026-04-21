07747654555c607d0f90fb13d0bf1dfb727dd098
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-21 14:53:22 +0900

    [Gold IV] Title: 사이클 게임, Time: 560 ms, Memory: 122780 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -5,51 +5,41 @@ import java.util.StringTokenizer;
 
 // 사이클 게임 
 public class Main {
-	static class Node{
-		Node parent;
-		int value;
-		
-		Node(int v){
-			this.value = v;
-		}
-		
-	}
-	
+	static int[] graph;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		Node[] graph = new Node[n];
+		graph = new int[n];
 		for(int i=0;i<n;i++) {
-			graph[i] = new Node(i);
-		} // 최대 백만번
-
+			graph[i] = i;
+		}
 		int t1, t2;
-		Node r1,r2;
+		int r1,r2;
 		for(int i=1;i<=m;i++) {
 			st = new StringTokenizer(br.readLine());
 			t1 = Integer.parseInt(st.nextToken());
 			t2 = Integer.parseInt(st.nextToken());
-			r1 = root(graph[t1]); r2 = root(graph[t2]);
+			r1 = root(t1); r2 = root(t2);
 			if(r1 == r2) {
 				System.out.println(i);
 				return ;
 			}
-			else if(r1.value < r2.value) {
+			else if(r1 < r2 ) {
 				// t1에 붙일게요
-				r2.parent = r1;
+				graph[r2] = r1;
 			} else {
 				// t2에 붙일게요
-				r1.parent = r2;
+				graph[r1] = r2;
 			}
 		}
 		System.out.println(0);
 	}
 	
-	public static Node root(Node node) {
-		if(node.parent == null) return node;
-		return root(node.parent);
+	public static int root(int value) {
+		if(graph[value] == value) return value;
+		return root(graph[value]);
 	}
 	
 }
