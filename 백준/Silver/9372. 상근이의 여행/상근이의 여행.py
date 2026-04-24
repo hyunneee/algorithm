7bf8fbacfea9f04c9e1e7b0ad2b1dfa8e24e0d43
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:39:28 +0900

    [Silver IV] Title: 상근이의 여행, Time: 320 ms, Memory: 135124 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,21 @@
+t = int(input())
+for _ in range(t):
+    n,m = map(int,input().split())
+
+    tree =[[] for _ in range(n+1)]
+    for _ in range(m):
+        a,b = map(int,input().split())
+        tree[a].append(b)
+        tree[b].append(a)
+    visit = [0] * (n+1)
+    def dfs(idx,cnt):
+        visit[idx] = 1
+        for i in tree[idx]:
+            if visit[i]==0:
+                cnt = dfs(i,cnt+1)
+        return cnt
+    result = dfs(1,0)
+
+    print(result)
+
+
