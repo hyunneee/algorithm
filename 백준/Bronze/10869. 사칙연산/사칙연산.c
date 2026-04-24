eddd6442d4d63a61aad1d6d75aaf2bf9607b921d
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:38:16 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    printf("%d\n",n+m);
+    printf("%d\n",n-m);
+    printf("%d\n",n*m);
+    printf("%d\n",n/m);
+    printf("%d\n",n%m);
+    return 0;
+}
