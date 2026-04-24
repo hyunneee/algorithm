f2599d0bf3c912ca4f4d5a4d5c9584cf00eeb576
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:44:18 +0900

    [Bronze I] Title: 기상청 인턴 신현수, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2435. 기상청 인턴 신현수/기상청 인턴 신현수.java
+++ 백준/Bronze/2435. 기상청 인턴 신현수/기상청 인턴 신현수.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int n=0, k=0; //변수 n,k 선언
-		int[] sum= new int[100]; //합들을 모아둘 배열 선언
-		int num; //더할 때 사용할 변수 선언
+		int n=0;
+		int k=0;
+		int[] sum= new int[100];
+		int num;
 		Scanner sc= new Scanner(System.in);
-		n = sc.nextInt(); //n,k 입력받기
-		k = sc.nextInt();
-		int[] pArr= new int[n]; //온도 받기 위한 배열 선언
-		for(int i=0;i<n;i++) { //n개의 온도를 받아주기 위한 반복문
+		n = sc.nextInt();
+		k=sc.nextInt();
+		int[] pArr= new int[n];
+		if(n==0) {
+			System.exit(0);
+			}
+		for(int i=0;i<n;i++) {
 			pArr[i]=sc.nextInt();
 		}
-		for(int i=0;i<=n-k;i++) { //연속적인 k일의 온도를 구하는 경우의 수 n-k+1
-			num=0; 
-			for(int j=i;j<i+k;j++) { 
+		for(int i=0;i<=n-k;i++) {
+			num=0;
+			for(int j=i;j<i+k;j++) {
 				num+=pArr[j];
 			}
 			sum[i]=num;
