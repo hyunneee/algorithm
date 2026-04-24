6f3b24bac3df2d8d36b66929e593fcf4028b132f
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:34:27 +0900

    [Gold V] Title: 회의실 배정, Time: 356 ms, Memory: 127408 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -7,12 +7,13 @@ schedule = []
 for _ in range(N):
     sp, ep = map(int, input().split(" "))
     schedule.append([sp, ep])
-schedule.sort(key=lambda x: (x[1],x[0]))
-s_p = float("INF")
+schedule.sort(key=lambda x: (x[1], x[0]))
+# print(schedule)
 e_p = float("-INF")
 count = 0
 for sp_i, ep_i in schedule:
     if e_p <= sp_i:
         e_p = ep_i
         count += 1
+        # print("sp_i, ep_i", sp_i, ep_i)
 print(count)
