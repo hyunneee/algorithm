68ac3432b7172e73a9a85d8e1517c7c323a81c17
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:32:57 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 316 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < n*2 ; j++) {
+				if(j > n+i) break;
+				if(n-i <= j && j <= n+i) {
+					System.out.print("*");					
+				} else {
+					System.out.print(" ");					
+				} 
+			}
+			System.out.println();
+		}
+	}
+}
