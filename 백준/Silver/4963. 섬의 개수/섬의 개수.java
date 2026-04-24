19c96b3c9e71972b21e2e9e874e691fe3737d02b
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:32:13 +0900

    [Silver II] Title: 섬의 개수, Time: 360 ms, Memory: 36340 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    static int w = 0, h=0, cnt=0;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0};
+    static int[] dc = {-1,  0,  1, 1, 1, 0,-1,-1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            w = sc.nextInt();
+            h = sc.nextInt();
+            cnt = 0;
+            if (w == 0 && h == 0) {
+                break;
+            }
+            map = new int[h][w];
+            visited = new boolean[h][w];
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    map[i][j] =sc.nextInt();
+                }
+            }
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if(map[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+
+            System.out.println(cnt);
+        }
+    }
+
+    public static void dfs(int i, int j){
+        int tr = 0, tc = 0;
+        visited[i][j] = true;
+        for(int k=0;k<8;k++){
+            tr = i+dr[k];
+            tc = j+dc[k];
+            if(0<=tr && tr <h && 0<=tc&& tc<w){
+                if(map[tr][tc] == 1 && !visited[tr][tc]) dfs(tr,tc);
+            }
+        }
+    }
+}
