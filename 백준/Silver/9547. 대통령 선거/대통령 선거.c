8b862ac4f2c24890704746dafb6abc8fa3e9829d
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:42:05 +0900

    [Silver III] Title: 대통령 선거, Time: 12 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/9547. 대통령 선거/대통령 선거.c
+++ 백준/Silver/9547. 대통령 선거/대통령 선거.c
@@ -1,63 +1,77 @@
-#include <stdio.h>
+#include<stdio.h>
 #pragma warning(disable:4996)
-
-int main(void){
-    int T;
-    scanf("%d",&T);
-    for(int t=0;t<T;t++){
-        int check[101]={0,};
-        int max=0;
-        int max2=0;
-        int max_idx=0;
-        int max2_idx=0;
-        int arr[101][101]={0,};
-        int cnt[101]={0,};
+int main()
+{
+    //freopen("test.inp", "r", stdin);
+    //freopen("test.out", "w", stdout);
+    int Test;
+    int num;
+    scanf("%d", &Test);
+    for (int i = 0; i < Test; i++)
+    {
+        int visit[101] = { 0, };
+        int max_index = 0;
+        int max = 0;
+        int second = 0;
+        int second_index = 0;
+        int arr[101][101] = { 0, };
+        int check[101] = { 0, };
         int C;
         int V;
-        
-        scanf("%d %d",&C, &V);
-        for(int i=0;i<V;i++){
-            for(int j=0;j<C;j++){
-                scanf("%d",&arr[i][j]);
+        scanf("%d %d", &C, &V);
+        for (int j = 0; j < V; j++)
+        {
+            for (int k = 0; k < C; k++)
+                scanf("%d", &arr[j][k]);
+        }
+        for (int j = 0; j < V; j++)
+        {
+            if (arr[j][0] != 0)
+                check[arr[j][0]]++;
+        }
+        for (int j = 0; j <=C; j++)
+        {
+            if (check[j] > max)
+            {
+                max = check[j];
+                max_index = j;
+            }    
+        }
+        for (int j = 0; j <= C; j++)
+        {
+            if (check[j] > second && second <= max && max_index!=j)
+            {
+                second = check[j];
+                second_index = j;
             }
         }
-        for(int i=0;i<V;i++){
-            if(arr[i][0]!=0)
-                cnt[arr[i][0]]++;
-        }
-        
-        for(int i=0;i<=C;i++){
-            if(cnt[i]>max){
-                max=cnt[i];
-                max_idx=i;}
-        }
-        for(int i=0;i<=C;i++){
-            if(cnt[i]>max2 && max2<=max && max_idx!=i){
-                max2=cnt[i];
-                max2_idx=i;
-            }
-        }
-        if(max>=(double)V/2)
-            printf("%d %d\n",max_idx,1);
-        else{
-            for(int i=1;i<C;i++){
-                for(int j=0;j<V;j++){
-                    if(arr[j][0]!=max_idx&&arr[j][0]!=max2_idx&&check[j]==0){
-                        if(arr[j][i]==max_idx){
+        if (max >= (double)V / 2)
+            printf("%d %d\n", max_index , 1);
+        else
+        {
+            for (int j = 1; j < C; j++)
+            {
+                for (int k = 0; k < V; k++)
+                {
+                    if (arr[k][0] != max_index && arr[k][0] != second_index && visit[k]==0)
+                    {
+                        if (arr[k][j] == max_index)
+                        {
                             max++;
-                            check[j]++;
+                            visit[k]++;
                         }
-                        else if(arr[j][i]==max2_idx){
-                            max2++;
-                            check[j]++;
+                        else if (arr[k][j] == second_index)
+                        {
+                            second++;
+                            visit[k]++;
                         }
                     }
                 }
             }
-            if(max>=(double)V/2)
-                printf("%d %d\n",max_idx,2);
-            else if(max2>=(double)V/2)
-                printf("%d %d\n",max2_idx,2);
-            }
+            if (max >= (double)V / 2)
+                printf("%d %d\n", max_index, 2);
+            else if (second >= (double)V / 2)
+                printf("%d %d\n", second_index, 2);
+        }
     }
 }
