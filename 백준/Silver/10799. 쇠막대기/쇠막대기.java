d336964c404db33548e95c374b2682cb633356fc
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:42:52 +0900

    [Silver II] Title: 쇠막대기, Time: 196 ms, Memory: 17220 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,14 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-//import java.io.BufferedWriter;
-//import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.Stack;
 
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String arr=br.readLine();
         char[] tmp=arr.toCharArray();
         /*
@@ -32,10 +32,9 @@ public class Main{
                 }
             }
         }
-        System.out.println(amount);
-        //bw.write(amount+"\n");
-        //bw.flush();
-        //bw.close();
-        //br.close();
+        bw.write(amount+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
