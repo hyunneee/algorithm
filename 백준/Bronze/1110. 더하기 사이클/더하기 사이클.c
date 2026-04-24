a7d83fade77ff37a9f04c07f6d478ef86ebf29b5
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:46:37 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.c
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.c
@@ -1,25 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main()
-{
-    int n;
-    int a,b,c;
-    int num;
-    scanf("%d",&n);
-    num=n;
-    int cycle=1;
-    while(1){
-        a=num/10;
-        b=num%10;
-        c=a+b; 
-        c%=10;
-        num=b*10+c;
-        if(n==num)
-            break;
-        else
-            cycle++;
-    }
-    printf("%d",cycle);
-
-    return 0;
+int main(void) {
+    int user,num,a1,a2,a3;
+    int count=0;
+    scanf("%d",&user);
+    num=user;
+    do{
+        a1=num/10;
+        a2=num%10;
+        a3=(a1+a2)%10;
+        count++;
+        num=a2*10+a3;
+    }while(user!=num);
+    printf("%d",count);
 }
