176363a90c66711ec92abebfe058a7c20d94979a
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:35:30 +0900

    [Gold IV] Title: 도서관, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+N, M = map(int, input().split(" "))
+qlist = list(map(int, input().split(" ")))
+nq = sorted([x for x in qlist if x < 0], reverse=True)
+pq = sorted([-x for x in qlist if x >= 0], reverse=True)
+
+cost = 0
+result = []
+value = 0
+while pq:
+    for i in range(0, M):
+        if i == 0:
+            cost = pq.pop()
+        else:
+            if len(pq) <= 0:
+                break
+            pq.pop()
+    heapq.heappush(result, abs(cost))
+
+while nq:
+    for i in range(0, M):
+        if i == 0:
+            cost = nq.pop()
+        else:
+            if len(nq) <= 0:
+                break
+            nq.pop()
+    heapq.heappush(result, abs(cost))
+
+while len(result) > 1:
+    value += heapq.heappop(result) * 2
+value += heapq.heappop(result)
+print(value)
