5d5a16665264cf4675fcf4e58950434dc24836e5
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:31:29 +0900

    [Silver I] Title: 카드 구매하기, Time: 252 ms, Memory: 21104 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] price = new int[N+1];
+		int[] dp = new int[N+1];
+		for(int i=1;i<=N;i++) {
+			price[i] = sc.nextInt();
+		}
+		for(int i=1;i<=N;i++) {
+			dp[i] = price[i];
+			for(int j=1;j<=i;j++) {
+				dp[i] = Math.max(dp[i], dp[i-j]+price[j]);
+			}
+		}
+		System.out.println(dp[N]);
+	}
+}
