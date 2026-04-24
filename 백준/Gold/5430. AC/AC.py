96a1f4e70af391cb3a59972c869f12256d73830e
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:42:27 +0900

    [Gold V] Title: AC, Time: 256 ms, Memory: 171020 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,27 +1,38 @@
-from collections import deque
 import sys
-T = int(sys.stdin.readline())
-for i in range(T):
-    flip = False
-    nonerror = True
-    p = sys.stdin.readline()
-    n = int(sys.stdin.readline())
-    listn = sys.stdin.readline().strip()
-    if n == 0: deq = deque()
-    else: deq = deque(map(int,listn[1:-1].split(",")))
+from collections import deque
+
+t= int(input())
+
+for i in range(t):
+    p=sys.stdin.readline().rstrip()
+    n=int(input())
+    arr=sys.stdin.readline().rstrip()[1:-1].split(",")
+    queue=deque(arr)
+
+    rev,front,back = 0,0,len(queue)-1
+    error=0
     
+    if n==0:
+        queue=[]
+        front=0
+        back=0
+
     for j in p:
-        if j == 'R':
-            flip = not flip
-        elif (j == 'D'):
-            if len(deq) <= 0:
-                nonerror=False
+        if j=='R':
+            rev+=1
+        elif j=='D':
+            if len(queue)<1:
+                error=1
+                print("error")
                 break
-            if(not flip):
-                deq.popleft()
             else:
-                deq.pop()
-    
-    if (flip): deq.reverse()
-    if(nonerror): print("["+",".join(map(str,deq))+"]")
-    else: print("error")
+                if rev%2==0:
+                    queue.popleft()
+                else:
+                    queue.pop()
+    if error==0:
+        if rev%2==0:
+            print("["+",".join(queue)+"]")
+        else:
+            queue.reverse()
+            print("["+",".join(queue)+"]")
