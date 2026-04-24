bd55ceff227e6bdf5cb312a10edfdf4735051928
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:37:31 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.c
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -3,12 +3,11 @@
 int main(void){
     int a;
     scanf("%d",&a);
-    if((a%400) && ((a%4) || !(a%100))){ //if(0)이면 거짓
-       printf("0\n"); 
+    if(a%400==0 || (a%4==0 && (a%100)!=0)){ //if(0)이면 거짓
+       printf("1\n"); 
     }
     else{
-        printf("1\n");
+        printf("0\n");
     }
-
     return 0;
 }
