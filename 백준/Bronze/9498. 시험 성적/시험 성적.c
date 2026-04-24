8132c0efab48251d43b51a34ce2f8d5aed20df52
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:37:58 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d",&n);
+    if(n>=90 && n<=100){
+        printf("A\n");
+    }
+    else if(n>=80){
+        printf("B\n");
+    }
+    else if(n>=70){
+        printf("C\n");
+    }else if(n>=60){
+        printf("D\n");
+    }
+    else{
+        printf("F\n");
+    }
+    
+    return 0;
+}
