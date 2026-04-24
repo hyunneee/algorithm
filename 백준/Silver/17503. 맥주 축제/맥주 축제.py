f84424dc2e0dcb275bf3b2a4683acee12c4f57ac
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-24 17:39:22 +0900

    [Silver I] Title: 맥주 축제, Time: 972 ms, Memory: 149428 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.py
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -1,30 +1,29 @@
 import sys
 import heapq
-
 input = sys.stdin.readline
-N, M, K = map(int, input().split(" "))
-s = []
-for k in range(K):
-    v, c = map(int, input().split(" "))
-    s.append((c, -v))
-s = sorted(s, reverse=True)
 
-vlist = []
-sum_v = 0
-while len(vlist) <= N:
-    if len(s) <= 0:
-        break
-    c, v = s.pop(-1)
-    heapq.heappush(vlist, (-v, c))
-    sum_v -= v
-    if len(vlist) == N:
-        if sum_v >= M:
+n,m,k = map(int,input().split())
+
+beerl = []
+pq = []
+heapq.heapify(pq)
+for _ in range(k):
+    v,c = map(int,input().split())
+    beerl.append((v,c))
+beerl.sort(key=lambda x:(x[1],x[0])) #도수레벨 -> 선호도 작은 
+find = False
+now_al = 0
+sum = 0
+for i in range(k):
+    heapq.heappush(pq,beerl[i][0])
+    sum+=beerl[i][0]
+    now_al = beerl[i][1]
+    if len(pq) == n:
+        if sum>= m:
+            find = True
+            print(now_al)
             break
         else:
-            v, c = heapq.heappop(vlist)
-            sum_v -= v
-if len(vlist) > N or sum_v < M:
-    print("-1")
-else:
-    max_c = max([c for v, c in vlist])
-    print(max_c)
+            sum -= heapq.heappop(pq)
+if not find:
+    print(-1)
