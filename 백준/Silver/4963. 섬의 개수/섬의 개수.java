2019a2bcdb4d9a061629371d43630137637ddd34
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-28 15:04:36 +0900

    [Silver II] Title: 섬의 개수, Time: 160 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,51 +1,61 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int w = 0, h=0, cnt=0;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0};
-    static int[] dc = {-1,  0,  1, 1, 1, 0,-1,-1};
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	static int w, h;
+	static int[][] map;
+	static boolean[][] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			if(w == 0 && h == 0) break;
+			map = new int[h][w];
+			visited = new boolean[h][w];
+			for(int i=0;i<h;i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0;j<w;j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-        while (true) {
-            w = sc.nextInt();
-            h = sc.nextInt();
-            cnt = 0;
-            if (w == 0 && h == 0) {
-                break;
-            }
-            map = new int[h][w];
-            visited = new boolean[h][w];
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    map[i][j] =sc.nextInt();
-                }
-            }
-
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if(map[i][j] == 1 && !visited[i][j]) {
-                        dfs(i, j);
-                        cnt++;
-                    }
-                }
-            }
-
-            System.out.println(cnt);
-        }
-    }
-
-    public static void dfs(int i, int j){
-        int tr = 0, tc = 0;
-        visited[i][j] = true;
-        for(int k=0;k<8;k++){
-            tr = i+dr[k];
-            tc = j+dc[k];
-            if(0<=tr && tr <h && 0<=tc&& tc<w){
-                if(map[tr][tc] == 1 && !visited[tr][tc]) dfs(tr,tc);
-            }
-        }
-    }
+			int cnt = 0;
+			for(int i=0;i<h;i++) {
+				for(int j=0;j<w;j++) {
+					if(map[i][j] == 1 && !visited[i][j]) {
+						cnt++;
+						bfs(i,j);
+					}
+				}
+			}
+			System.out.println(cnt);
+		}
+	}
+	
+	public static void bfs(int i,int j) {
+		int[] dr = {-1,-1,-1,0,1,1,1,0};
+		int[] dc = {-1,0,1,1,1,0,-1,-1};
+		
+		Queue<int[]> q = new ArrayDeque<>();
+		visited[i][j] = true;
+		q.add(new int[] {i,j});
+		while(!q.isEmpty()) {
+			int[] curr = q.poll();
+			for(int d=0;d<8;d++) {
+				int tr = curr[0] + dr[d];
+				int tc = curr[1] + dc[d];
+				if(tr < 0 || tr >= h || tc < 0 || tc >= w || visited[tr][tc] || map[tr][tc] == 0) continue;
+				visited[tr][tc] = true;
+				q.add(new int[] {tr,tc});
+			}
+		}
+	}
 }
